fc60c0a02aad88ea9f98a003d5ca55bec85c2dea
Shibumishra <sibumishra487@gmail.com>
2021-08-09 08:40:31 +0530

    change 404

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
 
 import './App.css';
-import {
-  BrowserRouter,
-  Route,
-  Redirect,
-  Switch
-} from 'react-router-dom';
+import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom';
 
 // Components
 import Container from './components/Container';
 import Nav from './components/Nav';
-import NotHere from './components/NotHere';
+// import NotHere from './components/NotHere';
 
 const App = () => (
   
@@ -24,8 +19,8 @@ const App = () => (
         <Route path="/search/:query" component={Container} />
         <Route path="/cats" render={ () => {return <Container query={'kittens'} /> }} />
         <Route path="/dogs" render={ () => {return <Container query={'puppys'} /> }} />
-        <Route path="/birds" render={ () => {return <Container query={'birds'} /> }} />
-        <Route component={NotHere} />
+        <Route path="/birds" render={ () => {return <Container query={'birds'} /> }} /> 
+        {/* <Route component={NotHere} /> */}
       </Switch> 
     </div>
   </BrowserRouter>
